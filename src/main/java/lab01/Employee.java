03d16a48531ef75f0faf1cd0d34366189a706036
Polcia <witkowska.pola@gmail.com>
2025-10-03 22:40:29 +0200

    update employee class

--- src/main/java/lab01/Employee.java
+++ src/main/java/lab01/Employee.java
@@ -7,6 +7,57 @@ public class Employee {
     private Position position;
     private int salary;
 
-    Employee(String fullName, String email, String company, Position position, int salary) {
+    Employee(String fullName, String email, Position position) {
+        this.fullName = fullName;
+        this.email = email;
+        this.company = "TechCorp";
+        this.position = position;
+        this.salary = position.getBaseSalary();
+    }
+
+    public String getName() {
+        return fullName;
+    }
+
+    public void setName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return email.equals(employee.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return email.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Full name: %s, email: %s, company: %s, position: %s, salary: %s zł".formatted(fullName, email, company, position, salary);
     }
 }
