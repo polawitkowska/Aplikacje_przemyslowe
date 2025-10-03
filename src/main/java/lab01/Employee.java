c3b179e4063b3c19fab8247054ea90d8ec11b8dd
Polcia <witkowska.pola@gmail.com>
2025-10-03 11:19:48 +0200

    first commit

---
+++ src/main/java/lab01/Employee.java
@@ -0,0 +1,12 @@
+package lab01;
+
+public class Employee {
+    private String fullName;
+    private String email;
+    private String company;
+    private Position position;
+    private int salary;
+
+    Employee(String fullName, String email, String company, Position position, int salary) {
+    }
+}
---
+++ src/main/java/lab01/Position.java
@@ -0,0 +1,25 @@
+package lab01;
+
+public enum Position {
+    PREZES(25000, 1),
+    WICEPREZES(18000, 2),
+    MANAGER(12000, 3),
+    PROGRAMISTA(8000, 4),
+    STAZYSTA(3000, 5);
+
+    private final int baseSalary;
+    private final int hierarchyLevel;
+
+    Position(int baseSalary, int hierarchyLevel) {
+        this.baseSalary = baseSalary;
+        this.hierarchyLevel = hierarchyLevel;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public int getHierarchyLevel() {
+        return hierarchyLevel;
+    }
+}
